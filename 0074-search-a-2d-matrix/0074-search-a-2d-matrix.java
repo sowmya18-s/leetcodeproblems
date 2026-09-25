3f239b6752c0af8f6a42212e937efecb29223acd
sowmya gudavalli <sowmyagudavalli17@gmail.com>
2026-09-25 11:29:59 +0530

    Time: 0 ms (100%), Space: 44 MB (24.15%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length,m=matrix[0].length;
+        int low=0,high=(m*n)-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/m,col=mid%m;
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
